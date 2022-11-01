01469383f6e5b3b068f84763db4a8b9bb50983cb
cesarlee38 <cesarlee38@hotmail.com>
2022-11-01 16:19:42 -0600

    Clase 4, tipos de datos y suma de dos numeros

---
+++ C4/C4.java
@@ -0,0 +1,9 @@
+public class C4{
+  public static void main(String args[]){
+   int num1 = 5;
+   int num2 = 2;
+   int resultado = 0;
+   resultado = num1 + num2;
+   System.out.println("El valor de la suma es: " + resultado);
+ }
+}
