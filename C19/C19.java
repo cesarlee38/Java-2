cb8a584070fcf2179ae9e1a7394b5c335e1f3a8e
cesarlee38 <cesarlee38@hotmail.com>
2022-11-02 17:57:50 -0600

    me faltaba la carpeta de la clase 19:V

---
+++ C19/C19.java
@@ -0,0 +1,19 @@
+public class C19{
+  public static void main(String args[]){
+
+   int arreglo[] = new int[5];
+
+   arreglo[0] = 5;
+   arreglo[1] = 220;
+   arreglo[2] = 8;
+   arreglo[3] = 458;
+   arreglo[4] = 22;
+
+   System.out.println("[" + arreglo[0] + "]");
+   System.out.println("[" + arreglo[1] + "]");
+   System.out.println("[" + arreglo[2] + "]");
+   System.out.println("[" + arreglo[3] + "]");
+   System.out.println("[" + arreglo[4] + "]");
+
+ }
+}
