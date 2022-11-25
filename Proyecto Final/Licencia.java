f829f435944a995c4ec1a32ee58b4fffb2a4b354
cesarlee38 <cesarlee38@hotmail.com>
2022-11-24 19:07:25 -0600

    Clase 43, formulario Licencia y usamos setEnabled en el jtextarea

---
+++ Proyecto Final/Licencia.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Licencia extends JFrame implements ActionListener, ChangeListener{
+
+  private JLabel l1, l2;
+  private JCheckBox cb1;
+  private JButton b1, b2;
+  private JScrollPane sp1;
+  private JTextArea ta1;
+
+  public Licencia(){
+
+   setLayout(null);
+   setTitle("Licencia");
+   setIconImage(new ImageIcon(getClass().getResource("images/icon.png")).getImage());
+
+   l1 = new JLabel("TÉRMINOS Y CONDICIONES");
+   l1.setBounds(215,5,200,30);
+   l1.setFont(new Font("Andale Mono", 1, 14));
+   l1.setForeground(new Color(0, 0, 0));
+   add(l1);
+
+   ta1 = new JTextArea();
+   ta1.setEditable(false);
+   ta1.setFont(new Font("Andale Mono", 0, 9));
+   ta1.setText("\n\n          TÉRMINOS Y CONDICIONES" +
+                "\n\n            A.  PROHIBIDA SU VENTA O DISTRIBUCIÓN SIN AUTORIZACIÓN DE LA GEEKIPEDIA DE ERNESTO." +
+                "\n            B.  PROHIBIDA LA ALTERACIÓN DEL CÓDIGO FUENTE O DISEÑO DE LAS INTERFACES GRÁFICAS." +
+                "\n            C.  LA GEEKIPEDIA DE ERNESTO NO SE HACE RESPONSABLE DEL MAL USO DE ESTE SOFTWARE." +
+                "\n\n          LOS ACUERDOS LEGALES EXPUESTOS ACONTINUACIÓN RIGEN EL USO QUE USTED HAGA DE ESTE SOFTWARE" +
+                "\n          (LA GEEKIPEDIA DE ERNESTO Y EL AUTOR ERNESTO), NO SE RESPONSABILIZAN DEL USO QUE USTED" + 
+                "\n          HAGA CON ESTE SOFTWARE Y SUS SERVICIOS. PARA ACEPTAR ESTOS TERMINOS HAGA CLIC EN (ACEPTO)" +
+                "\n          SI USTED NO ACEPTA ESTOS TERMINOS, HAGA CLIC EN (NO ACEPTO) Y NO UTILICE ESTE SOFTWARE." + 
+                "\n\n          PARA MAYOR INFORMACIÓN SOBRE NUESTROS PRODUCTOS O SERVICIOS, POR FAVOR VISITE" + 
+                "\n          http://www.youtube.com/ernestoperezm");
+   sp1 = new JScrollPane(ta1);
+   sp1.setBounds(10,40,575,200);
+   add(sp1);
+
+   cb1 = new JCheckBox("Yo Acepto");
+   cb1.setBounds(10,250,300,30);
+   cb1.addChangeListener(this);
+   add(cb1);
+
+   b1 = new JButton("Continuar");
+   b1.setBounds(10,290,100,30);
+   b1.addActionListener(this);
+   b1.setEnabled(false);
+   add(b1);
+
+   b2 = new JButton("No Acepto");
+   b2.setBounds(120,290,100,30);
+   b2.addActionListener(this);
+   b2.setEnabled(true);
+   add(b2);
+
+   ImageIcon image = new ImageIcon("images/coca-cola.png");
+   l2 = new JLabel(image);
+   l2.setBounds(315,135,300,300);
+   add(l2);
+
+ }
+
+  public void stateChanged(ChangeEvent e){
+   if(true){
+  }
+ }
+
+  public void actionPerformed(ActionEvent e){
+   if(e.getSource() == b1){
+  }
+   if(e.getSource() == b2){
+    System.exit(0);
+  }
+ }
+
+  public static void main(String args[]){
+
+   Licencia formulario = new Licencia();
+   formulario.setBounds(0,0,610,370);
+   formulario.setVisible(true);
+   formulario.setResizable(false);
+   formulario.setLocationRelativeTo(null);
+
+ }
+
+}
